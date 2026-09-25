335cf69ef63b7330cb633972551e5c72243a9ca9
AARYAN SHARMA <2500031465@kluniversity.in>
2026-09-25 17:09:15 +0530

    perf: read mobile state from media query subscription

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,19 +1,18 @@
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
+const MOBILE_QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    const mediaQuery = window.matchMedia(MOBILE_QUERY)
+    const onChange = () => setIsMobile(mediaQuery.matches)
+
+    onChange()
+    mediaQuery.addEventListener("change", onChange)
+    return () => mediaQuery.removeEventListener("change", onChange)
   }, [])
 
   return !!isMobile
